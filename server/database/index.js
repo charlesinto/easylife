f63cc80bd977a648182c78a1752111134d8152e8
charlesinto <charles.onuorah@yahoo.com>
2020-05-13 16:48:45 +0100

    correct bug in creating account with already existing email address

--- server/database/index.js
+++ server/database/index.js
@@ -1,7 +1,7 @@
 import mysql from 'mysql2';
 import "dotenv/config";
 
-// const {DB_DATABASE, DB_HOST, DB_PASSWORD, DB_USER} = process.env;
+const {DB_DATABASE, DB_HOST, DB_PASSWORD, DB_USER} = process.env;
 // console.log(DB_PASSWORD, DB_USER, DB_DATABASE, DB_HOST)
 const pool = mysql.createPool({
       host: DB_HOST,
