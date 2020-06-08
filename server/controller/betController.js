a4544e3e3fd3e6d619f8476fb9500d9b589130ba
charlesinto <charles.onuorah@yahoo.com>
2020-06-08 23:14:24 +0100

    user should be able to topup wallet

--- server/controller/betController.js
+++ server/controller/betController.js
@@ -1,5 +1,8 @@
 import { executeQuery } from "../util/helper"
+import PayStack from "paystack";
+import { p_SECRET_KEY } from "../config";
 
+const paystack = PayStack(p_SECRET_KEY)
 
 const getBetController = async (req, res) => {
     try{
@@ -51,7 +54,36 @@ const getUserWalletHistory = async (req, res) => {
 
 const topUpWallet = async (req, res) => {
     try{
+        const { transactionRef, amount: reqAmount} = req.body;
+        paystack.transaction.verify(transactionRef, async function(error, body){
+            if(error){
+                console.log(error);
+                return res.status(500).send({
+                    message: 'Some error were encountered',
+                    error
+                })
+            }
+            const {data: { amount, }} = body;
+            if(parseInt(reqAmount) != parseInt(amount)){
+                return res.status(400).send({
+                    message:'Payment mismatch',
+                })
+            }
+            const response = await executeQuery('select * from userWalletBalance where userId = ?', [req.user.id])
+            const balance = parseInt(response[0]['balance']);
+            const newBalance = balance + amount;
+            await executeQuery('update userWalletBalance set balance = ? where userId = ?', [newBalance, req.user.id])
 
+            await executeQuery(`insert into userWalletHistory(walletId, userId, transactionType, paymentChannel, paymentReference, amount) 
+                values (?,?,?,?,?,?);`, [response[0]['balance'],req.user.id, 'topup','paystack',transactionRef, reqAmount ])
+            
+            const response3 = await executeQuery('select * from userWalletBalance where userid = ?', [req.user.id])
+
+            return res.status(201).send({
+                message:'Operation Successful',
+                data: response3[0]
+            })
+        })
     }catch(error){
         console.log(error);
         return res.status(500).send({
--- server/routes/betRoute.js
+++ server/routes/betRoute.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import { verifyTokenMiddleWare } from '../util/helper';
-import { getUserWalletBalance, getUserWalletHistory } from '../controller/betController';
+import { getUserWalletBalance, getUserWalletHistory, topUpWallet } from '../controller/betController';
+import { verifyTopUpParams } from '../middleware/authMiddleware';
 
 const router = express.Router();
 
@@ -8,5 +9,7 @@ const router = express.Router();
 router.get('/user/wallet-balance', verifyTokenMiddleWare, getUserWalletBalance )
 
 router.get('/user/wallet-history', verifyTokenMiddleWare, getUserWalletHistory)
+
+router.post('/user/wallet-topup', verifyTokenMiddleWare, verifyTopUpParams, topUpWallet)
 
 export default router;
